af8b8c05dd933561980fb2f1379aff7dbbfacf74
Mauro Rosales <mauro.rosales247@gmail.com>
2022-09-18 09:18:30 -0700

    add behavior to gusScript, allowing game object to go to MC

--- Assets/Scripts/gusScript.cs
+++ Assets/Scripts/gusScript.cs
@@ -1,28 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class gusScript : MonoBehaviour
 {
-    //declare the transform of our goal (where the navmesh agent will move towards) and our navmesh agent (in this case our zombie)
-    private Transform goal;
-    private UnityEngine.AI.NavMeshAgent agent;
-    // Start is called before the first frame update
+    public Transform target;
+    public UnityEngine.AI.NavMeshAgent agent;
+    public Animator animator;
+    float dist;
+    
     void Start()
     {
-        //create references
-    goal = Camera.main.transform;
-    agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-    //set the navmesh agent's desination equal to the main camera's position (our first person character)
-    agent.destination = goal.position;
-    //start the walking animation
-    GetComponent<Animation>().Play ("Walk");
+    animator = GetComponent<Animator>();
+    target = GameObject.Find("Sphere").transform;
+    NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    dist = agent.remainingDistance;
+    animator.SetBool("isWalking", true);
 
     }
 
-    // Update is called once per frame
+    
+    
+    
     void Update()
+    
+  {
+
+    agent.SetDestination(target.position);
+    Check();
+  }
+
+  void Check()
+  {
+    if(Vector3.Distance(agent.destination, agent.transform.position) <= agent.stoppingDistance)
     {
-        
+      if(!agent.hasPath||agent.velocity.sqrMagnitude==0f)
+      {
+          print ("at destination");
+          animator.SetBool("isWalking", false);
+          
+      }
     }
+  }
+
+// void OnTriggerEnter (Collider col)
+
+//   {
+//     GetComponent<CapsuleCollider>().enabled = false;
+//     animator.SetBool("isWalking", false);
+//     //destroy this gus in six seconds.
+//     Destroy (gameObject, 6);
+//     //instantiate a new gus
+//     // GameObject gus = Instantiate(Resources.Load("Gus", typeof(GameObject))) as GameObject;
+
+//     // //set the coordinates for a new vector 3
+//     // float randomX = UnityEngine.Random.Range (-12f,12f);
+//     // float constantY = .01f;
+//     // float randomZ = UnityEngine.Random.Range (-13f,13f);
+//     // //set the guss position equal to these new coordinates
+//     // gus.transform.position = new Vector3 (randomX, constantY, randomZ);
+
+//     // //if the gus gets positioned less than or equal to 3 scene units away from the camera we won't be able to shoot it
+//     // //so keep repositioning the gus until it is greater than 3 scene units away. 
+//     // while (Vector3.Distance (gus.transform.position, Camera.main.transform.position) <= 3) {
+      
+//     //   randomX = UnityEngine.Random.Range (-12f,12f);
+//     //   randomZ = UnityEngine.Random.Range (-13f,13f);
+
+//     //   gus.transform.position = new Vector3 (randomX, constantY, randomZ);
+//     // }
+//     }
+
 }
